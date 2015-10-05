0aa9c313c66ea33dd5bf1d90bbc28e5989d8dedf
patriciocordeiro <paponcio6@gmail.com>
2015-10-05 16:36:23 -0300

    full screen productNavbar

--- public/components/navbar/productCatNavBar.controller.js
+++ public/components/navbar/productCatNavBar.controller.js
@@ -9,107 +9,247 @@
         // for open navigation tab on mouseover
         //        $scope.isTabActive = false;
 
+        //        vm.productNavCategories = [{
+        //            name: 'Celular & Tablet',
+        //            icon:'tablet',
+        //        }, {
+        //            name: 'Tv & Video',
+        //            icon:'tv',
+        //        }, {
+        //            name: 'Audio & Home Theater',
+        //            icon:'theaters',
+        //        }, {
+        //            name: 'Cameras Digitais',
+        //            icon:'photo_camera',
+        //        }, {
+        //            name: 'Cameras de Vídeo',
+        //            icon:'videocam',
+        //        }, {
+        //            name: 'Vídeo Games',
+        //            icon:'games',
+        //        },{ 
+        //            name: 'Informática',
+        //            icon:'laptop_mac'
+        //        }];
+        //        
+        var imgProductNavFolder = '../../assets/img/productNavBarSection/productsNavBarSubsectionImg/';
+
         vm.productNavCategories = [{
-            name: 'Celular & Tablet',
-            icon:'tablet',
+            name: 'Eletrônicos',
+            icon: 'tablet',
+            subcat: [{
+                name: 'Celulares & Tablets',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'CelularTablet.png',
+                subsubcat: [{
+                    name: 'tablet'
+                }, {
+                    name: 'dual chip'
+                }, {
+                    name: 'iphone'
+                }]
+            }, {
+                name: 'Cameras',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'CameraFotoVideo.png',
+                subsubcat: [{
+                    name: 'cam1'
+                }, {
+                    name: 'cam2'
+                }, {
+                    name: 'cam3'
+                }]
+            }, {
+                name: 'Tv & Home Theater',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'TvHomeTheater.png',
+                subsubcat: [{
+                    name: 'Tv1'
+                }, {
+                    name: 'Tv2'
+                }, {
+                    name: 'Tv3'
+                }]
+
+            }, {
+                name: 'Cameras de Vídeo',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'VideoCameras.png',
+                subsubcat: [{
+                    name: 'Video1'
+                }, {
+                    name: 'Video2'
+                }, {
+                    name: 'Video3'
+                }]
+
+            }, {
+                name: 'Som',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'Som.png',
+                subsubcat: [{
+                    name: 'Som1'
+                }, {
+                    name: 'Som2'
+                }, {
+                    name: 'Som3'
+                }]
+
+            }]
         }, {
-            name: 'Tv & Video',
-            icon:'tv',
-        }, {
-            name: 'Audio & Home Theater',
-            icon:'theaters',
-        }, {
-            name: 'Cameras Digitais',
-            icon:'photo_camera',
-        }, {
-            name: 'Cameras de Vídeo',
-            icon:'videocam',
-        }, {
-            name: 'Vídeo Games',
-            icon:'games',
-        },{ 
             name: 'Informática',
-            icon:'laptop_mac'
+            icon: 'laptop_mac',
+            subcat: [{
+                name: 'Notebook & NetBook',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'CelularTablet.png',
+                subsubcat: [{
+                    name: 'Notebook1'
+                }, {
+                    name: 'Notebook2'
+                }, {
+                    name: 'Notebook3'
+                }]
+            }, {
+                name: 'HD Externo',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'CameraFotoVideo.png',
+            }, {
+                name: 'Monitor',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'TvHomeTheater.png'
+
+            }, {
+                name: 'Workstation',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'VideoCameras.png'
+
+            }, {
+                name: 'Som ',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'Som.png'
+
+            }]
+        }, {
+            name: 'Eletrodomésticos',
+            icon: 'theaters',
+            subcat: [{
+                name: 'Celulares & Tablets'
+            }, {
+                name: 'Cameras'
+            }]
+
+        }, {
+            name: 'Ar & Ventilação',
+            icon: 'photo_camera',
+            subcat: [{
+                name: 'Celulares & Tablets',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'CelularTablet.png'
+            }, {
+                name: 'Cameras',
+                imgPath: imgProductNavFolder + 'eletronicos/' + 'CameraFotoVideo.png'
+
+            }]
+
+        }, {
+            name: 'Móveis',
+            icon: 'videocam',
+            subcat: ['Celulares & Tablets', 'Cameras']
+
+        }, {
+            name: 'Saúde & Beleza',
+            icon: 'games',
+        }, {
+            name: 'Entretenimento',
+            icon: 'laptop_mac',
+            subcat: ['Celulares & Tablets', 'Cameras']
+
         }];
+        //
+        //        vm.productNavSubcategories = [{
+        //            eletronicos: [
+        //                [{
+        //                    name: 'Tablets',
+        //                    imgPath: imgProductNavFolder + 'celular/' + 'MobilenavTablets.png'
+        //                }, {
+        //                    name: 'Multichip',
+        //                    imgPath: imgProductNavFolder + 'celular/' + 'celular_dual_chip.png'
+        //                }, {
+        //                    name: 'Desbloqueados',
+        //                    imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavsoundBars.png'
+        //                }],
+        //                [{
+        //                    name: 'Home Theather',
+        //                    imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavhomeTheaterSystems.png'
+        //
+        //                }, {
+        //                    name: 'Home Theather',
+        //                    imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavhomeTheaterSystems.png'
+        //
+        //                }, {
+        //                    name: 'Home Theather',
+        //                    imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavhomeTheaterSystems.png'
+        //
+        //                }],
+        //
+        //                [{
+        //                    name: 'TV LED',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnavAllTV.png'
+        //
+        //                }, {
+        //                    name: 'TV 3D',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'navtv3d.png'
+        //
+        //                }, {
+        //                    name: 'Smart TV',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnavAllTV.png'
+        //
+        //                }, {
+        //                    name: 'Full HD',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'LEDHDTVs.png'
+        //
+        //                }, {
+        //                    name: 'Ultra HD',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
+        //
+        //                }, {
+        //                    name: 'TV LCD',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
+        //
+        //                }, {
+        //                    name: 'TV Plasma',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
+        //
+        //                }, {
+        //                    name: 'TV Monitor',
+        //                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
+        //
+        //                }]
+        //            ]
+        //        }, {
+        //            informatica: [{
+        //                name: 'informatica 1',
+        //                imgPath: imgProductNavFolder + 'celular/' + 'MobilenavTablets.png'
+        //            }, {
+        //                name: 'informatica 2',
+        //                imgPath: imgProductNavFolder + 'celular/' + 'MobilenavTablets.png'
+        //            }, {
+        //                name: 'informatica 3',
+        //                imgPath: imgProductNavFolder + 'celular/' + 'MobilenavTablets.png'
+        //            }]
+        //        }];
+        //
+        vm.productNavSubcategories = [{
 
-        var imgProductNavFolder = '../../assets/img/productNav/'
-        vm.productNavSubcategories = [
-            [{
-                name: 'tablet',
-                imgPath: imgProductNavFolder + 'celular/' + 'MobilenavTablets.png'
+            //            name: 'eletronicos',
+            category: [{
+                //                name: 'celular&tablet',
+                itens: ['tablet', 'dual chip', 'iphone']
             }, {
-                name: 'Multichip',
-                imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavsoundBars.png'
+                //                name: 'cameras',
+                itens: ['digital', 'profisional', 'amador']
+            }]
+        }, {
+
+            //            name: 'informatica',
+            category: [{
+                //                name: 'computador',
+                itens: ['comp1', 'comp2']
             }, {
-                name: 'Desbloqueados',
-                imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavsoundBars.png'
-            }],
-            [{
-                name: 'Home Theather',
-                imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavhomeTheaterSystems.png'
-
-            }, {
-                name: 'Home Theather',
-                imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavhomeTheaterSystems.png'
-
-            }, {
-                name: 'Home Theather',
-                imgPath: imgProductNavFolder + 'celular/' + 'snyUSSubNavhomeTheaterSystems.png'
-
-            }],
-
-            [{
-                    name: 'TV LED',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnavAllTV.png'
-
-                }, {
-                    name: 'TV 3D',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'navtv3d.png'
-
-                }, {
-                    name: 'Smart TV',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnavAllTV.png'
-
-                }, {
-                    name: 'Full HD',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'LEDHDTVs.png'
-
-                }, {
-                    name: 'Ultra HD',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
-
-                }, {
-                    name: 'TV LCD',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
-
-                }, {
-                    name: 'TV Plasma',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
-
-                }, {
-                    name: 'TV Monitor',
-                    imgPath: imgProductNavFolder + 'tv-video/' + 'TVnav4k.png'
-
-                }
-                //             ,{
-                //                name: 'BluRay & DVD Players',
-                //                imgPath: imgProductNavFolder + 'tv-video/' + 'VideoBlurayDVDPlayers.png'
-                //
-                //            }, {
-                //                name: 'BluRay & DVD Players',
-                //                imgPath: imgProductNavFolder + 'tv-video/' + 'TVnavAcesssorios.png'
-                //
-                //            }, {
-                //                name: 'BluRay & DVD Players',
-                //                imgPath: imgProductNavFolder + 'tv-video/' + 'TVnavAcesssorios.png'
-                //
-                //            }, {
-                //                name: 'BluRay & DVD Players',
-                //                imgPath: imgProductNavFolder + 'tv-video/' + 'TVnavAcesssorios.png'
-                //
-                //            }
-            ]
-        ];
+                //                name: 'hd externo',
+                itens: ['hd1', 'hd2']
+            }]
+        }];
 
         //        $scope.productCategory = productCategory;
         //        console.log('catgreeting', $scope.productCategory.category)
@@ -122,12 +262,19 @@
             //            console.log('mudou para', productCategory.category)
         };
 
+        vm.getSelectedProductSection = function(selecteProductSection) {
+            productCategory.section = selecteProductSection;
+            console.log(productCategory.section);
+
+        }
+
         var productNavCatLength = vm.productNavCategories.length;
         vm.showSubMenu = new Array(productNavCatLength);
         var i = 0;
 
         while (i < productNavCatLength) {
-            vm.showSubMenu[i++] = true;
+            vm.showSubMenu[i++] = '';
+
         }
 
         console.log(vm.showSubMenu);
@@ -135,6 +282,19 @@
         vm.getSelectedSubMenu = function(index) {
             vm.showSubMenu[index] = !vm.showSubMenu[index];
             console.log(vm.showSubMenu);
+        }
+
+        vm.subCatisCollapsed = true;
+
+        /*Sub submenu controls*/
+        vm.subCatisCollapsed = [true, true, true, true, true];
+        var i = 0;
+        vm.collapseSubSubMenu = function(index, value) {
+            for (i = 0; i < productNavCatLength; i++) {
+                vm.subCatisCollapsed[i] = true;
+            }
+            vm.subCatisCollapsed[index] = !value;
+            console.log(vm.subCatisCollapsed)
         }
 
     };
--- public/services/getCategory.service.js
+++ public/services/getCategory.service.js
@@ -1,6 +1,9 @@
 angular.module('myApp').service('productCategory', function ProductCategory() {
     
     var productCategory = this;
+    
+    console.log("Servico productCategory INICIALIZADO");
     productCategory.category = '';
+    productCategory.section = '';
 
 })
