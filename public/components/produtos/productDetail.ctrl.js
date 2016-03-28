493b81bec5bfd8362fa1937cddba83c0e945f72c
patriciocordeiro <paponcio6@gmail.com>
2016-03-28 00:12:06 -0300

    Iniciado a pagina de detakhes do produto

--- public/components/produtos/productDetail.ctrl.js
+++ public/components/produtos/productDetail.ctrl.js
@@ -6,23 +6,23 @@
 
     function PrdDetailCtrl($rootScope, $cookies, $stateParams, $mdDialog, productSrvc) {
         var vm = this;
+		
         var prd = productSrvc;
         var prdId = $stateParams.id //product id
 
         vm.prdQty = 1;
-
         vm.dataChange = {
             value: 'false'
         };
 
         $rootScope.dataChange = 'false';
-
         //-----------------------------------------------------------
         //Get the selected product data
-        vm.prdData = prd.prdGetSingle(prd.prdData, prdId);
-
+        console.log(prd.prd.data);
+        vm.prdData = prd.prd.getDetails(prd.prd.data, prdId);
         //-----------------------------------------------------------
-
+       
+			
         //Get Compra
         vm.prdQty = prd.prdKartBuyQty //store the quantity (uses ng-model)
         vm.prdGetBuy = function(prdQty) {
@@ -31,5 +31,8 @@
             $rootScope.dataChange = !$rootScope.dataChange;
         }
         //-----------------------------------------------------------
+		/*Elevate zoom-------------------------------------*/
+		$("#zoom_01").elevateZoom();
+		//--------------------------------------------------
     }; //End of function PrdDetailCtrl
 }());
